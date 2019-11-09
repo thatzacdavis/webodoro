bcc84d2db104bbacdbbddcc2c7b738ea930f7bf7
Zac Davis <zac.davis@coxautoinc.com>
2019-11-09 11:27:43 -0500

    Format functions.js file

--- src/functions.js
+++ src/functions.js
@@ -1,12 +1,12 @@
 const prettyPrintTime = (string, pad, length) => {
-    return (new Array(length+1).join(pad)+string).slice(-length);
-}
+    return (new Array(length + 1).join(pad) + string).slice(-length);
+};
 
 const convertSecondsToReadableTime = (timeLeft) => {
-    let minutes = Math.floor(timeLeft / 60);
-    let seconds = timeLeft - minutes * 60;
-    
-    var finalTime = prettyPrintTime(minutes,'0',2) + ':'+ prettyPrintTime(seconds,'0',2);
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft - minutes * 60;
+
+    const finalTime = `${prettyPrintTime(minutes, '0', 2)}:${prettyPrintTime(seconds, '0', 2)}`;
 
     return finalTime;
 };
