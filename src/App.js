23cfe25a5a64b61f7f09b186805b00e16efb317d
Zac Davis <zac.davis@coxautoinc.com>
2019-11-09 11:28:07 -0500

    Format App.js

--- src/App.js
+++ src/App.js
@@ -1,45 +1,45 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { Container, Grid, Paper } from '@material-ui/core';
 import FunctionButton from './components/functionButton';
 import Timer from './components/timer';
-import { Container, Grid, Paper } from '@material-ui/core';
 
-const useStyles = makeStyles(theme => ({
-  root: {
-    flexGrow: 1,
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-  },
-  container: {
-    marginTop: '5em',
-  }
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+    container: {
+        marginTop: '5em',
+    },
 }));
 
 const App = () => {
-  const classes = useStyles();
-  const [timeLeft, setTimeLeft] = useState(1500);
+    const classes = useStyles();
+    const [timeLeft, setTimeLeft] = useState(1500);
 
-  return (
-    <Container className={classes.container}>
-      <Grid container spacing={3}>
-        <Grid item md={4}>
-          <Paper className={classes.paper} />
-        </Grid>
-        <Grid item md={4}>
-          <Paper className={classes.paper}>
-            <Timer timeLeft={timeLeft} setTimeLeft={setTimeLeft} />
-            <FunctionButton />
-          </Paper>
-        </Grid>
-        <Grid item md={4}>
-          <Paper className={classes.paper}/>
-        </Grid>
-      </Grid>
-    </Container>
-  );
-}
+    return (
+        <Container className={classes.container}>
+            <Grid container spacing={3}>
+                <Grid item md={4}>
+                    <Paper className={classes.paper} />
+                </Grid>
+                <Grid item md={4}>
+                    <Paper className={classes.paper}>
+                        <Timer timeLeft={timeLeft} setTimeLeft={setTimeLeft} />
+                        <FunctionButton />
+                    </Paper>
+                </Grid>
+                <Grid item md={4}>
+                    <Paper className={classes.paper} />
+                </Grid>
+            </Grid>
+        </Container>
+    );
+};
 
 export default App;
